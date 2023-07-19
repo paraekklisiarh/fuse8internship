0e38b4185124ed40f8a3a02dff682fc45a8ba899
Paraekkl <paraekklisiarh@gmail.com>
2023-07-20 01:19:22 +0700

    fix AccountProcessorBenchmark
    Заменены излишние общие переменные. Теперь тестируем только то, чем методы различаются.

--- Homework1/Benchmarks/AccountProcessorBenchmark.cs
+++ Homework1/Benchmarks/AccountProcessorBenchmark.cs
@@ -9,48 +9,19 @@ namespace Fuse8_ByteMinds.SummerSchool.Benchmarks;
 [RankColumn]
 public class AccountProcessorBenchmark
 {
-    private readonly List<BankAccount> _bankAccount = new();
-
-    /// <summary>
-    ///     Пытаюсь создать достаточную нагрузку, чтобы увидеть преимущество передачи структур по ссылке.
-    /// </summary>
-    [GlobalSetup]
-    public void GlobalSetup()
-    {
-        for (var i = 0; i < 100; i++)
-        {
-            var random = new Random();
-            _bankAccount.Add(new BankAccount
-            {
-                TotalAmount = random.Next(),
-                LastOperation = new BankOperation
-                {
-                    TotalAmount = random.Next(), OperationInfo0 = random.NextInt64(),
-                    OperationInfo1 = random.NextInt64(),
-                    OperationInfo2 = random.NextInt64()
-                },
-                PreviousOperation = new BankOperation
-                {
-                    TotalAmount = random.NextInt64(), OperationInfo0 = random.NextInt64(),
-                    OperationInfo1 = random.NextInt64(),
-                    OperationInfo2 = random.Next()
-                    
-                }
-            });
-        }
-    }
+    private readonly BankAccount _bankAccount = new();
 
     [Benchmark(Description = "Calculate with boxing")]
-    public List<decimal> Calculate()
+    public decimal Calculate()
     {
         AccountProcessor processor = new();
-        return _bankAccount.Select(bankAccount => processor.Calculate(bankAccount)).ToList();
+        return processor.Calculate(_bankAccount);
     }
 
     [Benchmark(Description = "Calculate with ref")]
-    public List<decimal> CalculatePerformed()
+    public decimal CalculatePerformed()
     {
         AccountProcessor processor = new();
-        return _bankAccount.Select(bankAccount => processor.CalculatePerformed(bankAccount)).ToList();
+        return processor.CalculatePerformed(in _bankAccount);
     }
 }

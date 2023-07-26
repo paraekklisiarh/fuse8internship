f216bacc98566fbe169fc95552f74e7fbd0e6d67
paraekklisiarh <paraekklisiarh@gmail.com>
2023-07-26 15:20:07 +0700

    Rewrite Lazy

--- Homework2/Domain/Lazy.cs
+++ Homework2/Domain/Lazy.cs
@@ -5,10 +5,29 @@
 /// </summary>
 public class Lazy<TValue>
 {
-	private readonly System.Lazy<TValue> _lazy;
-	public Lazy(Func<TValue> value)
-	{
-		_lazy = new System.Lazy<TValue>(value);
-	}
-	public TValue Value => _lazy.Value;
+    private TValue? _value;
+    
+    // Я попытался проверять _value на null, но это не сработало со значимыми типами
+    // и я не смог найти способ сделать genetic nullable
+    private bool _isValueInitialized;
+    
+    private readonly Func<TValue> _valueFactory;
+
+    public Lazy(Func<TValue> valueFactory)
+    {
+        _valueFactory = valueFactory;
+        _isValueInitialized = false;
+    }
+
+    public TValue? Value
+    {
+        get
+        {
+            if (_isValueInitialized) return _value;
+
+            _value = _valueFactory.Invoke();
+            _isValueInitialized = true;
+            return _value;
+        }
+    }
 }

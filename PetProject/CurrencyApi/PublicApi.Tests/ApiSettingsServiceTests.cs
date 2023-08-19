0639dfb586aae35a2642ffdc8fa8381b6576abbd
paraekklisiarh <paraekklisiarh@gmail.com>
2023-08-19 10:25:27 +0700

    [PublicAPI]: ApiSettings tests

---
+++ PetProject/CurrencyApi/PublicApi.Tests/ApiSettingsServiceTests.cs
@@ -0,0 +1,76 @@
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Models;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace PublicApi.Tests;
+
+public class ApiSettingsServiceTests
+{
+    private readonly AppDbContext _mockDbContext;
+    private readonly CurrencyApiSettings _apiSettingsMock;
+
+    private readonly ApiSettingsService _sut;
+
+    public ApiSettingsServiceTests()
+    {
+        var dbOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        _mockDbContext = new AppDbContext(dbOptions);
+
+        _apiSettingsMock = new CurrencyApiSettings(_mockDbContext);
+        
+        _sut = new ApiSettingsService(_apiSettingsMock);
+    }
+
+    public void Dispose()
+    {
+        _mockDbContext.Dispose();
+    }
+
+    [Fact]
+    public async Task DefaultCurrencySetAsync_SetValueSuccessfully()
+    {
+        // Arrange
+        var ct = CancellationToken.None;
+        var code = "RUB";
+
+        // Act
+        await _sut.DefaultCurrencySetAsync(code, ct);
+
+        var actual = await _apiSettingsMock.GetDefaultCurrencyAsync(ct);
+        
+        // Assert
+        
+        Assert.Equal(code, actual);
+    }
+
+    [Fact]
+    public async Task DefaultCurrencySetAsync_ThrowException_WhenNotValidCurrencyCode()
+    {
+        // Arrange
+        var ct = CancellationToken.None;
+        var code = "Абракадабра";
+
+        // Act and assert
+        await Assert.ThrowsAsync<CurrencyCodeValidationException>(() => _sut.DefaultCurrencySetAsync(code, ct));
+    }
+
+    [Fact]
+    public async Task RoundCountSetAsync_SetValueSuccessfully()
+    {
+        // Arrange
+        var ct = CancellationToken.None;
+        var count = 10;
+
+        // Act
+        await _sut.RoundCountSetAsync(count, ct);
+
+        var actual = await _apiSettingsMock.GetCurrencyRoundCountAsync(ct);
+        
+        // Assert
+        
+        Assert.Equal(count, actual);
+    }
+}
---
+++ PetProject/CurrencyApi/PublicApi.Tests/SettingsControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Controllers;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace PublicApi.Tests;
+
+public class SettingsControllerTests
+{
+    private readonly Mock<IApiSettingsService> _settingsServiceMock;
+
+    private readonly SettingsController _sut;
+
+    public SettingsControllerTests()
+    {
+        _settingsServiceMock = new Mock<IApiSettingsService>();
+        _sut = new SettingsController(_settingsServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task DefaultCurrencySet_ReturnOk()
+    {
+        // Arrange
+        var ct = CancellationToken.None;
+        var defaultCurrency = "RUB";
+
+        _settingsServiceMock.Setup(s => s.DefaultCurrencySetAsync(defaultCurrency, ct))
+            .Returns(Task.CompletedTask);
+
+        // Act
+
+        var result = await _sut.DefaultCurrencySet(defaultCurrency, ct);
+
+        // Assert
+        
+        Assert.IsType<OkResult>(result);
+    }
+
+    [Fact]
+    public async Task SetRoundCount_ReturnOk()
+    {
+        // Arrange
+        var ct = CancellationToken.None;
+        var count = -100;
+
+        _settingsServiceMock.Setup(s => s.RoundCountSetAsync(count, ct))
+            .Returns(Task.CompletedTask);
+
+        // Act
+
+        var result = await _sut.SetRoundCount(count, ct);
+
+        // Assert
+        
+        Assert.IsType<OkResult>(result);
+    }
+}
